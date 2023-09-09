c81801a05f2ce916343d96ae678728aade06c810
Родион Шукало <ban.zhmyh@yandex.ru>
2023-09-09 15:55:41 +0300

    code according to PEP8

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from api.views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
 
-
 router = DefaultRouter()
 
 router.register('posts', PostViewSet, basename='posts')
